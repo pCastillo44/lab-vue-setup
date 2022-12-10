4d75c8ec50ef258dfb588f0f956e1dea91befcc5
pCastillo44 <patri_alcarras@hotmail.com>
2022-12-10 16:29:52 +0100

    Vue setup

---
+++ vue-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="father">
+    <h2> Touch me!</h2>
+    <button @click="showCard">Show Card</button>
+  </div>
+  <br>
+  <Card :show="show" @closeFromChild="removeCard">
+  </Card>
+</template>
+<script setup>
+import {ref} from 'vue';
+import Card from "./components/Card.vue";
+const show = ref(false);
+console.log(show.value)
+function showCard() {
+  if (show.value == false) {
+    show.value = true
+  }
+  console.log(show.value)
+}
+function removeCard(x) {
+  console.log(x)
+  if (show.value == true) {
+    show.value = false
+  }
+  console.log(show.value)
+}
+</script>
+<style>
+.father {
+  border-radius:5px;
+  border:solid 3px blue;
+  margin:5% 5%;
+}
+.son {
+  border-radius:5px;
+  border:solid 3px red;
+  margin:5% 5%;
+}
+</style>
---
+++ vue-setup/src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="son" v-if="show">
+    <h2> Hello World</h2>
+    <button @click="removeCard">Remove Card</button>
+  </div>
+</template>
+<script setup>
+import {ref} from 'vue'
+const props = defineProps(["show"]);
+const emits = defineEmits(["closeFromChild"]);
+const beer = ref({
+    id: 1,
+    name: 'Pale Ale',
+    price: 50
+})
+function removeCard() {
+    emits('closeFromChild', beer.value)
+}
+</script>
+<style>
+</style>
